90139e2550c3a1b5d4421216778b885b4a1015d7
MIMIEYES <pierreluohao@gmail.com>
2019-08-24 21:44:59 +0800

    add comment

--- src/main/resources/archetype-resources/src/main/java/contract/DefaultContract.java
+++ src/main/resources/archetype-resources/src/main/java/contract/DefaultContract.java
@@ -2,6 +2,9 @@ package ${package}.contract;
 
 import io.nuls.contract.sdk.Contract;
 
+/**
+ * the sample class that can be changed or removed
+ */
 public class DefaultContract implements Contract {
 
     public DefaultContract(){
